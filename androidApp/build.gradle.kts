87207663361a7c4dfe59607317eb30a6ded3a59b
Diego Beraldin <diego.beraldin@gmail.com>
2023-09-09 15:10:52 +0200

    version 1.0.0-alpha05

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.diegoberaldin.raccoonforlemmy.android"
         minSdk = 26
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.0.0-alpha04"
+        versionCode = 4
+        versionName = "1.0.0-alpha05"
     }
     buildFeatures {
         compose = true
