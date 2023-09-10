679eeecf3b418a6cd130b52ebcbde11dba2de2ad
Diego Beraldin <diego.beraldin@gmail.com>
2023-09-10 21:07:11 +0200

    refactor(commonui): use material tab row

--- core-commonui/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/commonui/components/SectionSelector.kt
+++ core-commonui/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/commonui/components/SectionSelector.kt
@@ -1,28 +1,11 @@
 package com.github.diegoberaldin.raccoonforlemmy.core.commonui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.unit.dp
-import com.github.diegoberaldin.racconforlemmy.core.utils.onClick
-import com.github.diegoberaldin.raccoonforlemmy.core.appearance.theme.CornerSize
-import com.github.diegoberaldin.raccoonforlemmy.core.appearance.theme.Spacing
 
 @Composable
 fun SectionSelector(
@@ -31,69 +14,25 @@ fun SectionSelector(
     currentSection: Int,
     onSectionSelected: (Int) -> Unit,
 ) {
-    val highlightColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
-    Row(
-        modifier = modifier
-            .height(34.dp)
-            .padding(horizontal = Spacing.m)
-            .fillMaxWidth()
-            .border(
-                color = highlightColor,
-                width = 1.dp,
-                shape = RoundedCornerShape(CornerSize.m),
-            ),
-    ) {
-        titles.forEachIndexed { i, title ->
-            Surface(
-                color = Color.Transparent,
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .padding(bottom = Spacing.xxs)
-                    .onClick {
+    TabRow(
+        modifier = modifier,
+        selectedTabIndex = currentSection,
+        tabs = {
+            titles.forEachIndexed { i, title ->
+                Tab(
+                    selected = i == currentSection,
+                    text = {
+                        Text(
+                            text = title,
+                            style = MaterialTheme.typography.titleSmall,
+                            color = MaterialTheme.colorScheme.onSurface,
+                        )
+                    },
+                    onClick = {
                         onSectionSelected(i)
                     },
-            ) {
-                Box(
-                    modifier = Modifier.fillMaxSize().let {
-                        if (currentSection == i) {
-                            it.background(
-                                color = highlightColor,
-                                shape = when (i) {
-                                    0 -> RoundedCornerShape(
-                                        topStart = CornerSize.m,
-                                        bottomStart = CornerSize.m,
-                                    )
-
-                                    titles.lastIndex -> {
-                                        RoundedCornerShape(
-                                            topEnd = CornerSize.m,
-                                            bottomEnd = CornerSize.m,
-                                        )
-                                    }
-
-                                    else -> RectangleShape
-                                },
-                            )
-                        } else {
-                            it
-                        }
-                    },
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleSmall,
-                        color = MaterialTheme.colorScheme.onSurface,
-                    )
-                }
-            }
-            if (i < titles.lastIndex) {
-                Box(
-                    modifier = Modifier.width(1.dp).fillMaxHeight()
-                        .background(color = highlightColor),
                 )
             }
         }
-    }
+    )
 }
