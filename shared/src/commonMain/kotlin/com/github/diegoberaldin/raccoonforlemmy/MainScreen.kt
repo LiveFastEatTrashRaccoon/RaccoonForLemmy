c4ee208b3be6012748b9e53404f8f0ce3ba9918d
Diego Beraldin <diego.beraldin@gmail.com>
2023-09-10 10:36:50 +0200

    fix(home): bottom bar appearance when scrolling (#11)

--- shared/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/MainScreen.kt
+++ shared/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/MainScreen.kt
@@ -1,9 +1,7 @@
 package com.github.diegoberaldin.raccoonforlemmy
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -13,10 +11,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.TabNavigator
@@ -32,26 +35,26 @@ import com.github.diegoberaldin.raccoonforlemmy.feature.profile.ui.ProfileTab
 import com.github.diegoberaldin.raccoonforlemmy.feature.search.ui.SearchTab
 import com.github.diegoberaldin.raccoonforlemmy.feature.settings.ui.SettingsTab
 import com.github.diegoberaldin.raccoonforlemmy.ui.navigation.TabNavigationItem
+import kotlin.math.roundToInt
 
 internal class MainScreen : Screen {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         val themeRepository = remember { getThemeRepository() }
-        val isBottomBarVisible = remember { mutableStateOf(true) }
+        var bottomBarHeightPx by remember { mutableStateOf(0f) }
+        var bottomBarOffsetHeightPx by remember { mutableStateOf(0f) }
         val bottomBarNestedScrollConnection = remember {
             object : NestedScrollConnection {
                 override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                    if (available.y < -1) {
-                        isBottomBarVisible.value = false
-                    }
-                    if (available.y > 1) {
-                        isBottomBarVisible.value = true
-                    }
+                    val delta = available.y
+                    val newOffset = bottomBarOffsetHeightPx + delta
+                    bottomBarOffsetHeightPx = newOffset.coerceIn(-bottomBarHeightPx, 0f)
                     return Offset.Zero
                 }
             }
         }
+
         val bottomNavBarCoordinator = remember { getNavigationCoordinator() }
         LaunchedEffect(bottomNavBarCoordinator) {
             bottomNavBarCoordinator.setBottomBarScrollConnection(bottomBarNestedScrollConnection)
@@ -63,39 +66,39 @@ internal class MainScreen : Screen {
                     CurrentTab()
                 },
                 bottomBar = {
-                    AnimatedVisibility(
-                        visible = isBottomBarVisible.value,
-                        enter = slideInVertically(
-                            initialOffsetY = { it * 2 },
-                        ),
-                        exit = slideOutVertically(
-                            targetOffsetY = { it * 2 },
-                        ),
-                    ) {
-                        val themeState by themeRepository.state.collectAsState()
-                        val titleVisible by themeRepository.navItemTitles.collectAsState()
-                        BottomAppBar(
-                            contentPadding = PaddingValues(0.dp),
-                            backgroundColor = when (themeState) {
-                                ThemeState.Light -> {
-                                    md_theme_light_surface
-                                }
-
-                                ThemeState.Dark -> {
-                                    md_theme_dark_surface
-                                }
-
-                                else -> {
-                                    md_theme_black_surface
-                                }
+                    val themeState by themeRepository.state.collectAsState()
+                    val titleVisible by themeRepository.navItemTitles.collectAsState()
+                    BottomAppBar(
+                        modifier = Modifier
+                            .onGloballyPositioned {
+                                bottomBarHeightPx = it.size.toSize().height
+                            }
+                            .offset {
+                                IntOffset(
+                                    x = 0,
+                                    y = -bottomBarOffsetHeightPx.roundToInt()
+                                )
                             },
-                        ) {
-                            TabNavigationItem(HomeTab, withText = titleVisible)
-                            TabNavigationItem(SearchTab, withText = titleVisible)
-                            TabNavigationItem(ProfileTab, withText = titleVisible)
-                            TabNavigationItem(InboxTab, withText = titleVisible)
-                            TabNavigationItem(SettingsTab, withText = titleVisible)
-                        }
+                        contentPadding = PaddingValues(0.dp),
+                        backgroundColor = when (themeState) {
+                            ThemeState.Light -> {
+                                md_theme_light_surface
+                            }
+
+                            ThemeState.Dark -> {
+                                md_theme_dark_surface
+                            }
+
+                            else -> {
+                                md_theme_black_surface
+                            }
+                        },
+                    ) {
+                        TabNavigationItem(HomeTab, withText = titleVisible)
+                        TabNavigationItem(SearchTab, withText = titleVisible)
+                        TabNavigationItem(ProfileTab, withText = titleVisible)
+                        TabNavigationItem(InboxTab, withText = titleVisible)
+                        TabNavigationItem(SettingsTab, withText = titleVisible)
                     }
                 },
             )
