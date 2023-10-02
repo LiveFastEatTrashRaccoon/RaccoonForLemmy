deed22bcfd0c9e01ccda3fec61c188ad9451768b
Diego Beraldin <diego.beraldin@gmail.com>
2023-10-02 13:55:32 +0200

    version 1.0.0-beta13

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.github.diegoberaldin.raccoonforlemmy.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 22
-        versionName = "1.0.0-beta12"
+        versionCode = 23
+        versionName = "1.0.0-beta13"
 
         buildConfigField(
             "String",
