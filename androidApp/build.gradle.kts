905916e56f687a2ad24a1369a2f9aeefd5f909b5
Diego Beraldin <diego.beraldin@gmail.com>
2023-10-20 09:03:33 +0200

    version 1.0.0-beta21

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.diegoberaldin.raccoonforlemmy.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 30
-        versionName = "1.0.0-beta20"
+        versionCode = 31
+        versionName = "1.0.0-beta21"
 
         buildConfigField(
             "String",
