5d62b32d46fbc4a6606149c9294841c4dfd4e211
Diego Beraldin <diego.beraldin@gmail.com>
2023-10-21 08:30:11 +0200

    chore: improve shimmer effect background

--- core-utils/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/utils/ShimmerEffect.kt
+++ core-utils/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/utils/ShimmerEffect.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -14,18 +15,15 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntSize
 
 fun Modifier.shimmerEffect(
     duration: Int = 1000,
-    colors: List<Color> = listOf(
-        Color(0xFF333333),
-        Color(0xFF999999),
-        Color(0xFF333333),
-    ),
 ): Modifier = composed {
+    val c1 = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.1f)
+    val c2 = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.3f)
+    val colors = listOf(c1, c2, c1)
     var size by remember {
         mutableStateOf(IntSize.Zero)
     }
