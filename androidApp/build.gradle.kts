dfff1ed9fbe344c06cc8c5ac469e01cda8bfa5f6
Diego Beraldin <diego.beraldin@gmail.com>
2023-10-28 22:04:41 +0200

    version 1.0.0-RC2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.diegoberaldin.raccoonforlemmy.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 32
-        versionName = "1.0.0-RC1"
+        versionCode = 33
+        versionName = "1.0.0-RC2"
 
         buildConfigField(
             "String",
