6685aaff9d4dc337c5df9eabd4b78392815a865a
Diego Beraldin <diego.beraldin@gmail.com>
2023-11-27 23:02:39 +0100

    version 1.0.0-RC11

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.diegoberaldin.raccoonforlemmy.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 41
-        versionName = "1.0.0-RC10"
+        versionCode = 42
+        versionName = "1.0.0-RC11"
         archivesName.set("RaccoonForLemmy")
     }
     buildFeatures {
