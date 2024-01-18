75c2d6472fded08934974dfd122e46d730bed63d
Diego Beraldin <diego.beraldin@gmail.com>
2024-01-18 18:49:35 +0100

    chore: update email reporting address

--- unit/about/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/unit/about/AboutConstants.kt
+++ unit/about/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/unit/about/AboutConstants.kt
@@ -5,7 +5,7 @@ internal object AboutConstants {
         "https://github.com/diegoberaldin/RaccoonForLemmy/issues/new?labels=bug"
     const val CHANGELOG_URL =
         "https://github.com/diegoberaldin/RaccoonForLemmy/releases/latest"
-    const val REPORT_EMAIL_ADDRESS = "diego.beraldin+raccoon4lemmy@gmail.com"
+    const val REPORT_EMAIL_ADDRESS = "raccoonforlemmy@gmail.com"
     const val WEBSITE_URL = "https://github.com/diegoberaldin/RaccoonForLemmy"
     const val LEMMY_COMMUNITY_NAME = "raccoonforlemmy"
     const val LEMMY_COMMUNITY_INSTANCE = "lemmy.world"
