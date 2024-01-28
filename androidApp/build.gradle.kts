d86fd14b7fa5a6de4ae3c9c8e12e5661cc00e07c
Diego Beraldin <diego.beraldin@gmail.com>
2024-01-28 23:10:48 +0100

    version 1.7.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.diegoberaldin.raccoonforlemmy.android"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 72
-        versionName = "1.6.3"
+        versionCode = 73
+        versionName = "1.7.0"
         archivesName.set("RaccoonForLemmy")
     }
     buildFeatures {
