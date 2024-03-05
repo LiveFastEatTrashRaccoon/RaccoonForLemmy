32a5e0565aef0d9d5078216bbd009dfdd14171e2
Diego Beraldin <diego.beraldin@gmail.com>
2024-03-05 08:26:40 +0100

    fix: video resize mode

--- core/commonui/components/src/androidMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/commonui/components/VideoPlayer.kt
+++ core/commonui/components/src/androidMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/commonui/components/VideoPlayer.kt
@@ -3,6 +3,7 @@ package com.github.diegoberaldin.raccoonforlemmy.core.commonui.components
 import android.media.session.PlaybackState
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.FrameLayout
+import androidx.annotation.OptIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
@@ -12,6 +13,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
@@ -19,6 +21,7 @@ import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 
+@OptIn(UnstableApi::class)
 @Composable
 actual fun VideoPlayer(
     modifier: Modifier,
@@ -58,21 +61,21 @@ actual fun VideoPlayer(
             }
     }
 
-    DisposableEffect(
-        AndroidView(
-            modifier = modifier,
-            factory = {
-                PlayerView(context).apply {
-                    controllerAutoShow = true
-                    controllerHideOnTouch = true
-                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+    AndroidView(
+        modifier = modifier,
+        factory = {
+            PlayerView(context).apply {
+                controllerAutoShow = true
+                controllerHideOnTouch = true
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
 
-                    player = exoPlayer
-                    layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-                }
+                player = exoPlayer
+                layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
             }
-        )
-    ) {
+        }
+    )
+
+    DisposableEffect(Unit) {
         onDispose {
             exoPlayer.release()
         }
