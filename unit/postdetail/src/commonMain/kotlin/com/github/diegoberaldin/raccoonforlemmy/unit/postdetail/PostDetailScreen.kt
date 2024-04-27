3c7bf50de13fca661986435bac777bd45e0a23e2
Diego Beraldin <diego.beraldin@gmail.com>
2024-04-27 22:34:28 +0200

    fix: post detail navigation bar position (#731)

--- unit/postdetail/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/unit/postdetail/PostDetailScreen.kt
+++ unit/postdetail/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/unit/postdetail/PostDetailScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
@@ -439,7 +440,9 @@ class PostDetailScreen(
             if (uiState.currentUserId == null) {
                 return@Scaffold
             }
-            Box {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+            ) {
                 Column(
                     modifier = Modifier
                         .padding(padding)
