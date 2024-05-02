a7176db310c6131df6d812d4087014ec437eae67
Diego Beraldin <diego.beraldin@gmail.com>
2024-05-02 18:54:50 +0200

    enhancement: restore traditional FAB icon (#761)

--- core/commonui/components/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/commonui/components/FloatingActionButtonMenu.kt
+++ core/commonui/components/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/core/commonui/components/FloatingActionButtonMenu.kt
@@ -19,7 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -63,7 +63,7 @@ fun FloatingActionButtonMenu(
     val theme by themeRepository.uiTheme.collectAsState()
     val dynamicColors by themeRepository.dynamicColors.collectAsState()
     var fabExpanded by remember { mutableStateOf(false) }
-    val fabRotation by animateFloatAsState(if (fabExpanded) 180f else 0f)
+    val fabRotation by animateFloatAsState(if (fabExpanded) 45f else 0f)
     val enterTransition = remember {
         fadeIn(
             initialAlpha = 0.3f,
@@ -167,7 +167,7 @@ fun FloatingActionButtonMenu(
             content = {
                 Icon(
                     modifier = Modifier.rotate(fabRotation),
-                    imageVector = Icons.Default.Menu,
+                    imageVector = Icons.Default.Add,
                     contentDescription = null,
                     tint = MaterialTheme.colorScheme.onPrimaryContainer,
                 )
