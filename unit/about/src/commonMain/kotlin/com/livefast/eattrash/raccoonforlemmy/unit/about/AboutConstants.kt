c60f2651026b02022d9c900e9eaf1c0a843e883a
Christopher N7_X <n7x-dev@proton.me>
2024-08-05 22:12:37 -0400

    chore: update example URLs

--- unit/about/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/about/AboutConstants.kt
+++ unit/about/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/about/AboutConstants.kt
@@ -2,12 +2,12 @@ package com.livefast.eattrash.raccoonforlemmy.unit.about
 
 internal object AboutConstants {
     const val REPORT_URL =
-        "https://example.com/"
+        "https://github.com/LiveFastEatTrashRaccoon/RaccoonForLemmy/issues/new?labels=bug"
     const val CHANGELOG_URL =
-        "https://example.com"
-    const val REPORT_EMAIL_ADDRESS = "raccoonforlemmy@example.com"
-    const val WEBSITE_URL = "https://example.com"
+        "https://github.com/LiveFastEatTrashRaccoon/RaccoonForLemmy/releases/latest"
+    const val REPORT_EMAIL_ADDRESS = "livefast.eattrash.raccoon@gmail.com"
+    const val WEBSITE_URL = "https://github.com/LiveFastEatTrashRaccoon/RaccoonForLemmy"
     const val GOOGLE_PLAY_URL = "https://example.com"
-    const val LEMMY_COMMUNITY_NAME = "raccoonforlemmy"
+    const val LEMMY_COMMUNITY_NAME = "raccoonforlemmyapp"
     const val LEMMY_COMMUNITY_INSTANCE = "lemmy.world"
 }
