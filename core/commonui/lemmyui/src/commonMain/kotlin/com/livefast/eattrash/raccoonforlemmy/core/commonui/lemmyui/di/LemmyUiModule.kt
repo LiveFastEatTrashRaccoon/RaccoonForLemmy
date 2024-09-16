feca5bf05935e2e51f22486f3e0c67eafa19fd83
Akesi Seli <andongue.nena@gmail.com>
2024-09-16 23:37:03 +0200

    update dependency injection

--- core/commonui/lemmyui/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/commonui/lemmyui/di/LemmyUiModule.kt
+++ core/commonui/lemmyui/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/commonui/lemmyui/di/LemmyUiModule.kt
@@ -1,5 +1,7 @@
 package com.livefast.eattrash.raccoonforlemmy.core.commonui.lemmyui.di
 
+import com.livefast.eattrash.raccoonforlemmy.core.commonui.lemmyui.CustomUriHandler
+import com.livefast.eattrash.raccoonforlemmy.core.commonui.lemmyui.DefaultCustomUriHandler
 import com.livefast.eattrash.raccoonforlemmy.core.commonui.lemmyui.DefaultFabNestedScrollConnection
 import com.livefast.eattrash.raccoonforlemmy.core.commonui.lemmyui.FabNestedScrollConnection
 import org.koin.dsl.module
@@ -8,5 +10,13 @@ val lemmyUiModule =
     module {
         factory<FabNestedScrollConnection> {
             DefaultFabNestedScrollConnection()
+        }
+        single<CustomUriHandler> { params ->
+            DefaultCustomUriHandler(
+                fallbackHandler = params[0],
+                settingsRepository = get(),
+                detailOpener = get(),
+                customTabsHelper = get(),
+            )
         }
     }
