48f345bda592a1e753830f35edc3bc580837f2fd
Akesi Seli <andongue.nena@gmail.com>
2024-10-15 12:15:32 +0200

    replace deprecated methods in download

--- core/utils/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/gallery/Download.kt
+++ core/utils/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/gallery/Download.kt
@@ -6,11 +6,11 @@ import io.ktor.client.call.body
 import io.ktor.client.request.prepareGet
 import io.ktor.http.contentLength
 import io.ktor.utils.io.ByteReadChannel
-import io.ktor.utils.io.core.isEmpty
-import io.ktor.utils.io.core.readBytes
+import io.ktor.utils.io.readRemaining
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
+import kotlinx.io.readByteArray
 
 suspend fun GalleryHelper.download(url: String): ByteArray =
     withContext(Dispatchers.IO) {
@@ -21,8 +21,8 @@ suspend fun GalleryHelper.download(url: String): ByteArray =
             var result = byteArrayOf()
             while (!channel.isClosedForRead) {
                 val packet = channel.readRemaining(4096)
-                while (!packet.isEmpty) {
-                    val bytes = packet.readBytes()
+                while (!packet.exhausted()) {
+                    val bytes = packet.readByteArray()
                     result += bytes
                     println("Received ${result.size} bytes / ${httpResponse.contentLength()}")
                 }
