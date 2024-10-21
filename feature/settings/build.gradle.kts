3395dffca72ea80feb50bde275785065d4aadcf0
Akesi Seli <andongue.nena@gmail.com>
2024-10-21 22:32:12 +0200

    update Gradle scripts

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
                 implementation(projects.core.notifications)
                 implementation(projects.core.persistence)
                 implementation(projects.core.preferences)
+                implementation(projects.core.resources)
                 implementation(projects.core.utils)
 
                 implementation(projects.domain.lemmy.data)
