bb3dc391be8ccb975c1fdddd14b4cc5161e11851
Akesi Seli <andongue.nena@gmail.com>
2024-10-26 19:45:51 +0200

    expose current values in l10n manager

--- core/l10n/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/l10n/L10nManager.kt
+++ core/l10n/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/l10n/L10nManager.kt
@@ -4,6 +4,8 @@ import cafe.adriel.lyricist.Lyricist
 import com.livefast.eattrash.raccoonforlemmy.core.l10n.messages.Strings
 
 interface L10nManager {
+    val currentValues: Strings
+
     val lyricist: Lyricist<Strings>
 
     fun changeLanguage(lang: String)
