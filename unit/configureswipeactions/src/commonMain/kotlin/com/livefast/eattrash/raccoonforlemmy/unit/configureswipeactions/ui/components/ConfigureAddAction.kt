469a52f772c0dbf54a7d67533a8530a89b35da6e
Akesi Seli <andongue.nena@gmail.com>
2024-11-04 19:35:25 +0100

    fix add item row

--- unit/configureswipeactions/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/configureswipeactions/ui/components/ConfigureAddAction.kt
+++ unit/configureswipeactions/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/configureswipeactions/ui/components/ConfigureAddAction.kt
@@ -3,9 +3,11 @@ package com.livefast.eattrash.raccoonforlemmy.unit.configureswipeactions.ui.comp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Icon
@@ -14,6 +16,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import com.livefast.eattrash.raccoonforlemmy.core.appearance.theme.CornerSize
 import com.livefast.eattrash.raccoonforlemmy.core.appearance.theme.IconSize
 import com.livefast.eattrash.raccoonforlemmy.core.appearance.theme.Spacing
 import com.livefast.eattrash.raccoonforlemmy.core.appearance.theme.ancillaryTextAlpha
@@ -26,10 +30,13 @@ internal fun ConfigureAddAction(onAdd: () -> Unit) {
     Row(
         modifier =
             Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(CornerSize.xxl))
+                .onClick(onClick = onAdd)
                 .padding(
                     horizontal = Spacing.s,
                     vertical = Spacing.xs,
-                ).onClick(onClick = onAdd),
+                ),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -41,6 +48,7 @@ internal fun ConfigureAddAction(onAdd: () -> Unit) {
         )
         Spacer(modifier = Modifier.width(Spacing.xs))
         Text(
+            modifier = Modifier.weight(1f),
             text = LocalStrings.current.buttonAdd,
             style = MaterialTheme.typography.labelMedium,
         )
