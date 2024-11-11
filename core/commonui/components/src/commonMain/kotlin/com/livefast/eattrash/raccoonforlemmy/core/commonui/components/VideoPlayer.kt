1ca433ca0b0e6eab79bd86fa481de9e0ec803fe0
Akesi Seli <andongue.nena@gmail.com>
2024-11-11 08:32:42 +0100

    rewrite video player using new library

--- core/commonui/components/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/commonui/components/VideoPlayer.kt
+++ core/commonui/components/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/commonui/components/VideoPlayer.kt
@@ -1,15 +1,22 @@
 package com.livefast.eattrash.raccoonforlemmy.core.commonui.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import chaintech.videoplayer.ui.video.VideoPlayerView
+import com.livefast.eattrash.raccoonforlemmy.core.resources.di.getCoreResources
 
-/*
- * CREDITS:
- * https://www.droidcon.com/2023/07/31/unifying-video-players-compose-multiplatform-for-ios-android-desktop/
- */
 @Composable
-expect fun VideoPlayer(
+fun VideoPlayer(
     url: String,
     modifier: Modifier = Modifier,
-    onPlaybackStarted: (() -> Unit)? = null,
-)
+    contentScale: ContentScale = ContentScale.FillWidth,
+) {
+    val resources = remember { getCoreResources() }
+    VideoPlayerView(
+        modifier = modifier,
+        url = url,
+        playerConfig = resources.getPlayerConfig(contentScale),
+    )
+}
