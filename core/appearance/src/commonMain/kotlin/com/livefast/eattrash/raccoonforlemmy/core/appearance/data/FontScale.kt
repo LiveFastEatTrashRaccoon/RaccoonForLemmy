d9b3d7f242fd139e26446d1fa87dea86a697bb84
Akesi Seli <andongue.nena@gmail.com>
2024-11-13 23:02:05 +0100

    enhancement: review font scale factors

--- core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/appearance/data/FontScale.kt
+++ core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/appearance/data/FontScale.kt
@@ -55,11 +55,11 @@ fun Float.toFontScale(): FontScale =
     }
 
 private object ReferenceValues {
-    const val LARGEST = 1.075f
-    const val LARGER = 1.05f
-    const val LARGE = 1.025f
-    const val NORMAL = 1f
-    const val SMALL = 0.985f
-    const val SMALLER = 0.95f
-    const val SMALLEST = 0.925f
+    const val LARGEST = 1.5f
+    const val LARGER = 1.36f
+    const val LARGE = 1.23f
+    const val NORMAL = 1.1f
+    const val SMALL = 0.96f
+    const val SMALLER = 0.83f
+    const val SMALLEST = 0.7f
 }
