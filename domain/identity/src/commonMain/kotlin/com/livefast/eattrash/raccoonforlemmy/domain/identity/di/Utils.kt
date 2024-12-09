e4061edfd0cbcb2be1f2d2c17350e03b2b958279
Akesi Seli <andongue.nena@gmail.com>
2024-12-09 22:28:58 +0100

    add getCustomUriHandler util

--- domain/identity/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/identity/di/Utils.kt
+++ domain/identity/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/identity/di/Utils.kt
@@ -1,5 +1,9 @@
 package com.livefast.eattrash.raccoonforlemmy.domain.identity.di
 
+import androidx.compose.ui.platform.UriHandler
 import com.livefast.eattrash.raccoonforlemmy.domain.identity.repository.ApiConfigurationRepository
+import com.livefast.eattrash.raccoonforlemmy.domain.identity.urlhandler.CustomUriHandler
 
 expect fun getApiConfigurationRepository(): ApiConfigurationRepository
+
+expect fun getCustomUriHandler(fallbackUriHandler: UriHandler): CustomUriHandler
