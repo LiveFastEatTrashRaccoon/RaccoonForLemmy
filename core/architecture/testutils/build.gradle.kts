36ff15b4120b9265aee610451f473ebc57b2f251
Akesi Seli <andongue.nena@gmail.com>
2024-12-09 14:14:40 +0100

    chore(build): apply convention plugins to :core:architecture:testutils

--- core/architecture/testutils/build.gradle.kts
+++ core/architecture/testutils/build.gradle.kts
@@ -1,29 +1,8 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    id("com.livefast.eattrash.kotlinMultiplatform")
 }
 
-@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    applyDefaultHierarchyTemplate()
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "core.architecture.testutils"
-            isStatic = true
-        }
-    }
-
     sourceSets {
         val commonMain by getting
         val androidMain by getting {
@@ -33,19 +12,5 @@ kotlin {
                 implementation(kotlin("test-junit"))
             }
         }
-    }
-}
-
-android {
-    namespace = "com.livefast.eattrash.raccoonforlemmy.core.architecture.testutils"
-    compileSdk =
-        libs.versions.android.compileSdk
-            .get()
-            .toInt()
-    defaultConfig {
-        minSdk =
-            libs.versions.android.minSdk
-                .get()
-                .toInt()
     }
 }
