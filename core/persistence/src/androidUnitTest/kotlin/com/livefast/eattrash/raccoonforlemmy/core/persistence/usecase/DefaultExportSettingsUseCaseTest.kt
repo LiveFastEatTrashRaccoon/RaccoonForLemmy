ed7aa91a030ab60821d4fd3ca1b69030899f3db1
Dieguitux <andongue.nena@gmail.com>
2024-12-10 23:29:25 +0100

    chore: add missing tests to :core:persistence (#156)
    * exclude provider package
    
    * add tests for import/export use cases
    
    * add update test for anonymous in settings repository

---
+++ core/persistence/src/androidUnitTest/kotlin/com/livefast/eattrash/raccoonforlemmy/core/persistence/usecase/DefaultExportSettingsUseCaseTest.kt
@@ -0,0 +1,30 @@
+package com.livefast.eattrash.raccoonforlemmy.core.persistence.usecase
+
+import com.livefast.eattrash.raccoonforlemmy.core.persistence.data.SettingsModel
+import com.livefast.eattrash.raccoonforlemmy.core.persistence.repository.SettingsRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.encodeToString
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DefaultExportSettingsUseCaseTest {
+    private val settingsRepository = mockk<SettingsRepository>()
+    private val sut = DefaultExportSettingsUseCase(settingsRepository)
+
+    @Test
+    fun whenInvoked_thenResultIsAsExpected() =
+        runTest {
+            val originalSettings = SettingsModel()
+            every { settingsRepository.currentSettings } returns MutableStateFlow(originalSettings)
+
+            val res = sut()
+
+            val expected = jsonSerializationStrategy.encodeToString(originalSettings.toData())
+            assertEquals(expected, res)
+            verify { settingsRepository.currentSettings }
+        }
+}
