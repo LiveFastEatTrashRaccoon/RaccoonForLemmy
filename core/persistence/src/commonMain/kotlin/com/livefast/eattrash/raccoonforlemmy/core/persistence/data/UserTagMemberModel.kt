8cf44b80e8da5b5c9dc13b302db9e4d760ad3a3f
Akesi Seli <andongue.nena@gmail.com>
2024-12-29 11:44:49 +0100

    define domain models

---
+++ core/persistence/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/persistence/data/UserTagMemberModel.kt
@@ -0,0 +1,11 @@
+package com.livefast.eattrash.raccoonforlemmy.core.persistence.data
+
+data class UserTagMemberModel(
+    val username: String,
+    val userTagId: Long,
+)
+
+data class UserTagModel(
+    val id: Long? = null,
+    val name: String,
+)
