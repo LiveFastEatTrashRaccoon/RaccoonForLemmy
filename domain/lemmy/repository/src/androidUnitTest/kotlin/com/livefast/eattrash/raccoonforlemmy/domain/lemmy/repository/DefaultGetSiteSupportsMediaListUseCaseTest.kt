b4e94def82f897faec7de3170de3737de3584078
Akesi Seli <andongue.nena@gmail.com>
2025-01-04 11:10:49 +0100

    chore: add test for DefaultGetSiteSupportsMediaListUseCase

---
+++ domain/lemmy/repository/src/androidUnitTest/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/lemmy/repository/DefaultGetSiteSupportsMediaListUseCaseTest.kt
@@ -0,0 +1,35 @@
+package com.livefast.eattrash.raccoonforlemmy.domain.lemmy.repository
+
+import com.livefast.eattrash.raccoonforlemmy.core.testutils.DispatcherTestRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class DefaultGetSiteSupportsMediaListUseCaseTest {
+    @get:Rule
+    val dispatcherTestRule = DispatcherTestRule()
+
+    private val isSiteVersionAtLeastUseCase =
+        mockk<IsSiteVersionAtLeastUseCase> {
+            coEvery { execute(any(), any(), any(), any()) } returns true
+        }
+    private val sut =
+        DefaultGetSiteSupportsMediaListUseCase(
+            isSiteVersionAtLeastUseCase = isSiteVersionAtLeastUseCase,
+        )
+
+    @Test
+    fun whenInvoked_thenResultAndInteractionsAreAsExpected() =
+        runTest {
+            val res = sut()
+
+            assertTrue(res)
+            coVerify {
+                isSiteVersionAtLeastUseCase.execute(major = 0, minor = 19, patch = 4)
+            }
+        }
+}
