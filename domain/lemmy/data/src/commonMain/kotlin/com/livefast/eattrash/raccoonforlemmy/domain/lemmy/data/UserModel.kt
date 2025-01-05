4f7d89edafc6c214964dca46e9abb9e6e1cba08b
Akesi Seli <andongue.nena@gmail.com>
2025-01-05 23:34:15 +0100

    add extension to populate special tags

--- domain/lemmy/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/lemmy/data/UserModel.kt
+++ domain/lemmy/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/lemmy/data/UserModel.kt
@@ -1,6 +1,9 @@
 package com.livefast.eattrash.raccoonforlemmy.domain.lemmy.data
 
+import androidx.compose.runtime.Composable
+import com.livefast.eattrash.raccoonforlemmy.core.l10n.LocalStrings
 import com.livefast.eattrash.raccoonforlemmy.core.persistence.data.UserTagModel
+import com.livefast.eattrash.raccoonforlemmy.core.persistence.data.UserTagType
 import kotlin.jvm.Transient
 
 data class UserModel(
@@ -38,3 +41,73 @@ val UserModel.readableHandle: String
                 append("@$host")
             }
         }
+
+@Composable
+fun UserModel.populateSpecialTags(
+    isAdmin: Boolean,
+    isBot: Boolean,
+    isMe: Boolean,
+    isMod: Boolean,
+    isOp: Boolean,
+    adminColor: Int? = null,
+    botColor: Int? = null,
+    meColor: Int? = null,
+    opColor: Int? = null,
+    modColor: Int? = null,
+): UserModel =
+    this.let { user ->
+        user.copy(
+            tags =
+                buildList {
+                    // first add all special tags
+                    if (isAdmin) {
+                        add(
+                            UserTagModel(
+                                type = UserTagType.Admin,
+                                name = LocalStrings.current.defaultTagAdmin,
+                                color = adminColor,
+                            ),
+                        )
+                    }
+                    if (isBot) {
+                        add(
+                            UserTagModel(
+                                type = UserTagType.Bot,
+                                name = LocalStrings.current.defaultTagBot,
+                                color = botColor,
+                            ),
+                        )
+                    }
+                    if (isMe) {
+                        add(
+                            UserTagModel(
+                                type = UserTagType.Me,
+                                name = LocalStrings.current.defaultTagCurrentUser,
+                                color = meColor,
+                            ),
+                        )
+                    }
+                    if (isMod) {
+                        add(
+                            UserTagModel(
+                                type = UserTagType.Moderator,
+                                name = LocalStrings.current.defaultTagModerator,
+                                color = modColor,
+                            ),
+                        )
+                    }
+                    if (isOp) {
+                        add(
+                            UserTagModel(
+                                type = UserTagType.OriginalPoster,
+                                name = LocalStrings.current.defaultTagOriginalPoster,
+                                color = opColor,
+                            ),
+                    )
+                }
+
+                // then add all the regular tags
+                addAll(tags)
+            },
+        )
+    }
