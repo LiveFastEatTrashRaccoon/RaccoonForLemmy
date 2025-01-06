67fbd75acc7e518c3f2fb13aeda08a67208d8226
Akesi Seli <andongue.nena@gmail.com>
2025-01-06 12:40:36 +0100

    version 1.14.1-beta01

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,8 +22,8 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 141
-        versionName = "1.14.0"
+        versionCode = 142
+        versionName = "1.14.1-beta01"
     }
     base.archivesName = "RaccoonForLemmy"
     buildFeatures {
