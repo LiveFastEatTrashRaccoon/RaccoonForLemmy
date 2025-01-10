3b6ef2356bd364110ac8d88c5ccde1c92cb9f0de
Akesi Seli <andongue.nena@gmail.com>
2025-01-10 22:27:39 +0100

    update build scripts

--- core/api/build.gradle.kts
+++ core/api/build.gradle.kts
@@ -9,11 +9,12 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(libs.kodein)
-                implementation(libs.ktorfit.lib)
-                implementation(libs.ktor.serialization)
                 implementation(libs.ktor.contentnegotiation)
                 implementation(libs.ktor.json)
                 implementation(libs.ktor.logging)
+                implementation(libs.ktor.serialization)
+                implementation(libs.ktorfit.converters.response)
+                implementation(libs.ktorfit.lib)
 
                 implementation(projects.core.utils)
             }
