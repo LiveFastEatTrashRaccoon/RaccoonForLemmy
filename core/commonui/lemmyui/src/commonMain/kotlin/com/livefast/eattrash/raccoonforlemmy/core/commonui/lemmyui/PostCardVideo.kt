12b63dd5a5a8994db7bc7b4b3a36a4b53918dc12
Akesi Seli <andongue.nena@gmail.com>
2025-01-13 10:29:22 +0100

    enhancement: use video preview in post cards, with play button to open full screen

--- core/commonui/lemmyui/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/commonui/lemmyui/PostCardVideo.kt
+++ core/commonui/lemmyui/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/commonui/lemmyui/PostCardVideo.kt
@@ -1,17 +1,16 @@
 package com.livefast.eattrash.raccoonforlemmy.core.commonui.lemmyui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Fullscreen
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,11 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.livefast.eattrash.raccoonforlemmy.core.appearance.theme.Spacing
-import com.livefast.eattrash.raccoonforlemmy.core.commonui.components.VideoPlayer
+import com.livefast.eattrash.raccoonforlemmy.core.commonui.components.VideoPlayerPreview
 import com.livefast.eattrash.raccoonforlemmy.core.l10n.LocalStrings
 
 @Composable
@@ -49,66 +49,48 @@ fun PostCardVideo(
                 .clipToBounds(),
         contentAlignment = Alignment.Center,
     ) {
-        if (blurred) {
-            Column(
-                modifier = Modifier.padding(vertical = Spacing.s),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(Spacing.s),
+        var shouldBeRendered by remember(autoLoadImages) { mutableStateOf(autoLoadImages) }
+        if (shouldBeRendered) {
+            VideoPlayerPreview(
+                modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .blur(radius = if (blurred) 60.dp else 0.dp),
+                url = url,
+            )
+
+            FilledIconButton(
+                colors =
+                    IconButtonDefaults.filledIconButtonColors().copy(
+                        containerColor = MaterialTheme.colorScheme.onBackground,
+                    ),
+                modifier =
+                    Modifier
+                        .align(Alignment.Center)
+                        .padding(
+                            start = Spacing.xs,
+                        ),
+                onClick = {
+                    onOpenFullScreen?.invoke()
+                },
             ) {
-                Text(
-                    text = LocalStrings.current.messageVideoNsfw,
-                    style = MaterialTheme.typography.bodyMedium,
+                Icon(
+                    imageVector = Icons.Default.PlayArrow,
+                    contentDescription = LocalStrings.current.actionOpenFullScreen,
                 )
-                Button(
-                    onClick = {
-                        onOpen?.invoke()
-                    },
-                ) {
-                    Text(
-                        text = LocalStrings.current.buttonLoad,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                }
             }
         } else {
-            var shouldBeRendered by remember(autoLoadImages) { mutableStateOf(autoLoadImages) }
-            if (shouldBeRendered) {
-                VideoPlayer(
-                    modifier = Modifier.fillMaxSize(),
-                    url = url,
-                    contentScale = ContentScale.Fit,
+            Button(
+                modifier = Modifier.padding(vertical = Spacing.s),
+                onClick = {
+                    shouldBeRendered = true
+                },
+            ) {
+                Text(
+                    text = LocalStrings.current.buttonLoad,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
                 )
-
-                IconButton(
-                    modifier =
-                        Modifier
-                            .align(Alignment.TopStart)
-                            .padding(
-                                start = Spacing.xs,
-                            ),
-                    onClick = {
-                        onOpenFullScreen?.invoke()
-                    },
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Fullscreen,
-                        contentDescription = LocalStrings.current.actionOpenFullScreen,
-                    )
-                }
-            } else {
-                Button(
-                    modifier = Modifier.padding(vertical = Spacing.s),
-                    onClick = {
-                        shouldBeRendered = true
-                    },
-                ) {
-                    Text(
-                        text = LocalStrings.current.buttonLoad,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                }
             }
         }
     }
