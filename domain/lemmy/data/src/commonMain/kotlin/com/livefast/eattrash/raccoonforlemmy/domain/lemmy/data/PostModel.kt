a6b3359659ff2c735c920b0f707778fc95b93641
Dieguitux <andongue.nena@gmail.com>
2025-01-14 23:06:05 +0100

    fix: simplify video and image post URL matching (#265)

--- domain/lemmy/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/lemmy/data/PostModel.kt
+++ domain/lemmy/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/domain/lemmy/data/PostModel.kt
@@ -33,9 +33,7 @@ data class PostModel(
 )
 
 val PostModel.imageUrl: String
-    get() =
-        thumbnailUrl?.takeIf { it.isNotEmpty() }
-            ?: url?.takeIf { it.looksLikeAnImage }.orEmpty()
+    get() = (url?.takeIf { it.looksLikeAnImage } ?: thumbnailUrl).orEmpty()
 
 val PostModel.videoUrl: String
-    get() = url?.takeIf { it.looksLikeAVideo }?.takeIf { it.isNotEmpty() }.orEmpty()
+    get() = url?.takeIf { it.looksLikeAVideo }.orEmpty()
