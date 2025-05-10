40d157fc425a2cab605bd66feb451cbbfd58c20c
Akesi Seli <andongue.nena@gmail.com>
2025-05-10 16:09:18 +0200

    add explicit binding for Coil3 PlatformContext on iOS

---
+++ core/utils/src/iosMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/di/NativeImageLoadModule.kt
@@ -0,0 +1,14 @@
+package com.livefast.eattrash.raccoonforlemmy.core.utils.di
+
+import coil3.PlatformContext
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.singleton
+
+internal actual val nativeImageLoadModule = DI.Module("NativeImageLoadModule") {
+    bind<PlatformContext> {
+        singleton {
+            PlatformContext.INSTANCE
+        }
+    }
+}
