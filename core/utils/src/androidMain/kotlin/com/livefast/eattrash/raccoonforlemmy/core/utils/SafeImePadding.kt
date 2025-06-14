0bb75e919c568ff0814995af90123f81eeb4b8ac
Dieguitux <andongue.nena@gmail.com>
2025-06-14 20:01:13 +0200

    apply fix from Raccoon for Friendica for IME padding (#446)

--- core/utils/src/androidMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/SafeImePadding.kt
+++ core/utils/src/androidMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/SafeImePadding.kt
@@ -1,8 +1,13 @@
 package com.livefast.eattrash.raccoonforlemmy.core.utils
 
 import android.os.Build
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 
 /*
  * There is a weird bug on Android Q and below due to which the IME padding is added twice and
@@ -10,11 +15,20 @@ import androidx.compose.ui.Modifier
  *
  * This workaround simply ignores the imePadding() modifier in those cases.
  */
+@Composable
 actual fun Modifier.safeImePadding(): Modifier =
     then(
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Modifier.imePadding()
         } else {
-            Modifier
+            // custom approach for older android versions
+            val insets = WindowInsets.ime
+            val density = LocalDensity.current
+
+            Modifier.padding(
+                bottom = with(density) {
+                    (insets.getBottom(density) * 0.5f).toDp()
+                },
+            )
         },
     )
--- core/utils/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/SafeImePadding.kt
+++ core/utils/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/utils/SafeImePadding.kt
@@ -1,5 +1,7 @@
 package com.livefast.eattrash.raccoonforlemmy.core.utils
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@Composable
 expect fun Modifier.safeImePadding(): Modifier
