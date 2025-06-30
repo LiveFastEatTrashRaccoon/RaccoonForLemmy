4475dc32c5e8dd636b13ac787e44b58540cbc065
Dieguitux <andongue.nena@gmail.com>
2025-06-30 16:19:29 +0200

    migrate :unit:configurenavbar

--- unit/configurenavbar/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/configurenavbar/di/ConfigureNavBarModule.kt
+++ unit/configurenavbar/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/configurenavbar/di/ConfigureNavBarModule.kt
@@ -1,24 +1,20 @@
 package com.livefast.eattrash.raccoonforlemmy.unit.configurenavbar.di
 
-import com.livefast.eattrash.raccoonforlemmy.unit.configurenavbar.ConfigureNavBarMviModel
+import com.livefast.eattrash.raccoonforlemmy.core.architecture.di.bindViewModel
 import com.livefast.eattrash.raccoonforlemmy.unit.configurenavbar.ConfigureNavBarViewModel
 import org.kodein.di.DI
-import org.kodein.di.bind
 import org.kodein.di.instance
-import org.kodein.di.provider
 
 val configureNavBarModule =
     DI.Module("ConfigureNavBarModule") {
-        bind<ConfigureNavBarMviModel> {
-            provider {
-                ConfigureNavBarViewModel(
-                    accountRepository = instance(),
-                    identityRepository = instance(),
-                    bottomNavItemsRepository = instance(),
-                    settingsRepository = instance(),
-                    hapticFeedback = instance(),
-                    notificationCenter = instance(),
-                )
-            }
+        bindViewModel {
+            ConfigureNavBarViewModel(
+                accountRepository = instance(),
+                identityRepository = instance(),
+                bottomNavItemsRepository = instance(),
+                settingsRepository = instance(),
+                hapticFeedback = instance(),
+                notificationCenter = instance(),
+            )
         }
     }
