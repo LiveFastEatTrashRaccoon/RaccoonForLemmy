e9d115311635202cb66b304ad0be5fd8ea94bddf
Dieguitux <andongue.nena@gmail.com>
2025-07-04 17:15:15 +0200

    migrate :unit:myaccount

--- unit/myaccount/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/myaccount/di/MyAccountModule.kt
+++ unit/myaccount/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/myaccount/di/MyAccountModule.kt
@@ -1,31 +1,27 @@
 package com.livefast.eattrash.raccoonforlemmy.unit.myaccount.di
 
-import com.livefast.eattrash.raccoonforlemmy.unit.myaccount.ProfileLoggedMviModel
+import com.livefast.eattrash.raccoonforlemmy.core.architecture.di.bindViewModel
 import com.livefast.eattrash.raccoonforlemmy.unit.myaccount.ProfileLoggedViewModel
 import org.kodein.di.DI
-import org.kodein.di.bind
 import org.kodein.di.instance
-import org.kodein.di.provider
 
 val myAccountModule =
     DI.Module("MyAccountModule") {
-        bind<ProfileLoggedMviModel> {
-            provider {
-                ProfileLoggedViewModel(
-                    identityRepository = instance(),
-                    apiConfigurationRepository = instance(),
-                    postPaginationManager = instance(),
-                    commentPaginationManager = instance(),
-                    postRepository = instance(),
-                    commentRepository = instance(),
-                    themeRepository = instance(),
-                    settingsRepository = instance(),
-                    shareHelper = instance(),
-                    notificationCenter = instance(),
-                    hapticFeedback = instance(),
-                    postNavigationManager = instance(),
-                    lemmyValueCache = instance(),
-                )
-            }
+        bindViewModel {
+            ProfileLoggedViewModel(
+                identityRepository = instance(),
+                apiConfigurationRepository = instance(),
+                postPaginationManager = instance(),
+                commentPaginationManager = instance(),
+                postRepository = instance(),
+                commentRepository = instance(),
+                themeRepository = instance(),
+                settingsRepository = instance(),
+                shareHelper = instance(),
+                notificationCenter = instance(),
+                hapticFeedback = instance(),
+                postNavigationManager = instance(),
+                lemmyValueCache = instance(),
+            )
         }
     }
