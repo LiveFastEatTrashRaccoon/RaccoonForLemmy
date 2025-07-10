5c07637872b8794e70d38237002d6f6aef3b064c
Dieguitux <andongue.nena@gmail.com>
2025-07-10 22:09:20 +0200

    expose observable flows in AccountDao

--- core/persistence/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/persistence/dao/AccountDao.kt
+++ core/persistence/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/core/persistence/dao/AccountDao.kt
@@ -2,13 +2,18 @@ package com.livefast.eattrash.raccoonforlemmy.core.persistence.dao
 
 import app.cash.sqldelight.Query
 import com.livefast.eattrash.raccoonforlemmy.core.persistence.AccountEntity
+import kotlinx.coroutines.flow.Flow
 
 interface AccountDao {
     fun getAll(): Query<AccountEntity>
 
+    fun observeAll(): Flow<List<AccountEntity>>
+
     fun getBy(username: String?, instance: String?): Query<AccountEntity>
 
     fun getActive(): Query<AccountEntity>
+
+    fun observeActive(): Flow<AccountEntity?>
 
     fun create(username: String, instance: String, jwt: String?, avatar: String?)
 
